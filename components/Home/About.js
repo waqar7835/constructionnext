1956a5d1903d348c19ba17f66b301113442d4c2f
paulriviera6 <priviera6@gmail.com>
2021-02-21 16:28:20 +0500

    add about section in home page

---
+++ components/Home/About.js
@@ -0,0 +1,60 @@
+const About = () => {
+  return (
+    <div className="about-area section-space-less30e">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div className="video-box-layout1">
+              <img src="images/about-4-1.png" alt="img" />
+              <a
+                className="primary-icon popup-youtube video-play-button"
+                href="https://youtu.be/O_C5CN1L3Xo"
+              >
+                <i className="icofont icofont-play"></i>
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div className="about-content-layout1">
+              <h2>
+                About The <span>Fabricate</span>
+              </h2>
+              <p>
+                <strong>
+                  {" "}
+                  Fabrication generates objects in Ruby. Fabricators are
+                  schematics for your objects, and can be created as needed
+                  anywhere in your app or specs.
+                </strong>
+              </p>
+              <ul>
+                <li>
+                  <a href="#">Alienum phaedrum torquatos necu</a>
+                </li>
+                <li>
+                  <a href="#">Nihil expetendis in mei</a>
+                </li>
+                <li>
+                  <a href="#">Mei an pericula euripidispartem</a>
+                </li>
+              </ul>
+              <p className="off-mobile">
+                but has specific support for ActiveRecord Models, Mongoid
+                Documents. Sequel Models Sequel Modelsbut has for specific.
+                support for ActiveRecord Models, Mongoid Documents, ActiveRecord
+                Fabrication can generate anything
+              </p>
+              <div className="button-for-more text-left">
+                <a href="#" title="learn More" className="btn btn-str-up2">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- components/Home/index.js
+++ components/Home/index.js
@@ -1,11 +1,16 @@
 import Slider from './Slider'
 import Welcome from './Welcome'
+import About from './About'
+
+
+
 const Index = () => {
   return (
     <div className="">
       
       <Slider />
       <Welcome />
+      <About />
     </div>
   );
 };
