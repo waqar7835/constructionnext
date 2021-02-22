f9971459d916e86e8ffce73ef2bd7d402de7426d
waqar7835 <waqar7835@gmail.com>
2021-02-22 17:34:18 +0500

    data fetched for detail page

--- pages/products/detail.js
+++ pages/products/detail.js
@@ -1,11 +1,38 @@
 import Layout from "@components/Layout";
+import Head from "next/head";
+import { baseURL } from "@config/config";
 import SingleProduct from "@components/Products/SingleProduct/Main";
-const Index = () => {
+const Index = ({ content }) => {
   return (
     <Layout>
-      <SingleProduct />
+       <Head>
+          {content && (
+            <>
+              <title>{`${content.title}`}</title>
+              <meta
+                property="og:title"
+                content={`${content.title}`}
+              />
+              <meta property="og:site_name" content="Qatar Living" />
+              <meta property="og:type" content="website" />
+              <meta
+                property="og:title"
+                content={`${content.title}`}
+              />
+            </>
+          )}
+        </Head>
+      <SingleProduct content = { content }/>
     </Layout>
   );
+};
+Index.getInitialProps = async ({ query }) => {
+  let targetURL = `${baseURL}/api/product-detail/${query.id}?_format=hal_json`;
+  const result = await fetch(targetURL);
+  const data = await result.json();
+  return {
+    content: !!data ? data[0] : {},
+  };
 };
 
 export default Index;
