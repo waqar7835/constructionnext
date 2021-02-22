4e1f4a4faba07e05716b5d71d87e7121565296d3
unknown <kanwalnafees96@gmail.com>
2021-02-22 03:58:12 -0800

    multi checkboxes

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import Equipments from '@components/Common/Equipments';
-=======
 import Equipments from '../Common/Equipments';
->>>>>>> e62a77841f0bee7ed99849e29138658cca30076c
 import List from './List';
 import Filters from './Filters';
 import EmailBlock from './Email_Block';
