526a650cda7dde6774a873ca2f48944ad1e64cbb
waqar7835 <waqar7835@gmail.com>
2021-02-22 21:26:14 +0500

    changes for making menu work

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://demo.ajsoftsol.com';
+export const baseURL = 'https://demo.ajsoftsol.com';
 export const baseHeaders = {
   "Content-Type": "application/hal+json",
 };
