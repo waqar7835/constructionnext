efd9e1eb91ebcd89c7aa2f738a4439729d226939
paulriviera6 <priviera6@gmail.com>
2021-02-23 12:20:08 +0500

    working on menu

--- components/Layout/MainMenu.js
+++ components/Layout/MainMenu.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Container , Navbar, Nav ,NavLink } from "react-bootstrap";
 import Link from "next/link";
 const MainMenu = () => {
   return (
@@ -17,46 +17,33 @@ const MainMenu = () => {
     //   </Navbar.Collapse>
     // </Navbar>
     // </div>
-    // <nav id="dropdown">
-    //   <ul className="text-left">
-    //     <li>
-    //       <Link href="/">
-    //         <a>Home</a>
-    //       </Link>
-    //     </li>
+    <nav id="dropdown">
+      <ul className="text-left">
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
 
-    //     <li>
-    //       <a href="#">Products</a>
-    //       <ul>
-    //         <li>
-    //           <Link href="/inventory/search">
-    //             <a>All Products</a>
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </li>
+        <li>
+          <a href="#">Products</a>
+          <ul>
+            <li>
+              <Link href="/inventory/search">
+                <a>All Products</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
 
-    //     <li>
-    //       <Link href="/contact">
-    //         <a>Contact</a>
-    //       </Link>
-    //     </li>
-    //   </ul>
-    // </nav>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a className="nav-item nav-link" href="#">Features</a>
-        <a className="nav-item nav-link" href="#">Pricing</a>
-        <a className="nav-item nav-link disabled" href="#">Disabled</a>
-      </div>
-    </div>
-  </nav>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+     
   );
 };
 
